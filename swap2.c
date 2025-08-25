37e8839ff0335a15e55e39902a4002f8ebb16312
Rajat Negi <007negirajat@gmail.com>
2025-08-25 10:56:37 +0530

    swap2.c

--- swap2.c
+++ swap2.c
@@ -11,4 +11,22 @@ Input 2:
 Output 2:
 After swap: 14 7
 */
+#include<stdio.h>
+int main ()
+{
+int num1,num2 ;
+//ask user to input two numbers
+printf("enter two number: ");
+scanf("%d%d",&num1,&num2);
+ 
+printf("before swapping a=%d b=%d \n",num1,num2);
+
+num1=num1-num2;// Step 1: Subtract b from a and store result in a
+num2=num2+num1;// Step 2: Add new a (which is a-b) to b → this gives original a
+num1=num2-num1; // Step 3: Subtract new a (a-b) from new b (original a) → gives original b
+ 
+printf("after swapping a=%d b=%d \n" ,num1,num2);//display vlaues after swapping
+return 0;
+}
+
 
