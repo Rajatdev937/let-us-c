6af0d7e868a88394172f97dcb6e2f97ef5c25c34
Rajat Negi <007negirajat@gmail.com>
2025-10-14 22:29:42 +0530

    Add Day51.c and Day52.c

--- loops/address.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main ()
-{
-printf("rishikesh road\nathoorwala,jolly grant\nBhaniywala\ndehradun\n ");
-return 0;
-}
--- loops/sum.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-int a = 5;
-printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",a++,a++,++a,a++,++a,a,a++,++a);
-return 0;
-}
