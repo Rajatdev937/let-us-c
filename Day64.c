3077391650d3dd67011f498938df5981281f7658
Rajat Negi <007negirajat@gmail.com>
2025-10-29 08:56:31 +0530

    new push

---
+++ Day64.c
@@ -0,0 +1,62 @@
+// Q114: Write a program to take a string s as input. The task is to find the length of the longest substring without repeating characters. Print the length as output.
+
+/*
+Sample Test Cases:
+Input 1:
+s = "abcabcbb"
+Output 1:
+3
+
+Input 2:
+s = "bbbbb"
+Output 2:
+1
+
+Input 3:
+s = "pwwkew"
+Output 3:
+3
+
+*/
+
+#include <stdio.h>
+
+// Function to sort the array using Bubble Sort
+void sortArray(int arr[], int n) {
+    int temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, k;
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Enter k: ");
+    scanf("%d", &k);
+    
+    // Sort the array
+    sortArray(arr, n);
+    
+    // kth smallest element will be at index k-1 after sorting
+    printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+    
+    return 0;
+}
